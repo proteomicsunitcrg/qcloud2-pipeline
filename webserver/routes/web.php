fcdff71cfe539ecb8fcacdee2135dd2bd8efdba5
Toni Hermoso Pulido <toniher@cau.cat>
2018-06-26 17:18:19 +0200

    endsWith

--- webserver/routes/web.php
+++ webserver/routes/web.php
@@ -100,9 +100,3 @@ function isOutputFileOK( $output ) {
 }
 
 
-function endsWith( $haystack, $needle ) {
-    $length = strlen( $needle );
-
-    return $length === 0 || ( substr($haystack, -$length) === $needle );
-}
-
