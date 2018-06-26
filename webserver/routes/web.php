f4f3a2c56c286b983556c235b4ed0cda94bee916
Toni Hermoso Pulido <toniher@cau.cat>
2018-06-26 15:05:45 +0200

    problem slashes

--- webserver/routes/web.php
+++ webserver/routes/web.php
@@ -18,10 +18,10 @@ $router->get('/', function(\Illuminate\Http\Request $request){
         if ( file_exists( $inputdir."/".$inputfile ) ) {
             
             $outputfile = str_replace( ".raw", "", $inputfile );
-            $opts = '--32 --mzML --zlib --filter "peakPicking true 1-"';
+            $opts = "--32 --mzML --zlib --filter \"peakPicking true 1-\"";
             
             if ( $request->has('opts') ) {
-                $opts = $request->input('opts');
+                $opts = addslashes( $request->input('opts') );
             }
             
             $command =  env('QCLOUD_EXEC_PATH')." ".$inputdir."/".$inputfile." ".$opts." --outfile ".$outputfile." -o ".env('QCLOUD_OUTPUT_PATH');
