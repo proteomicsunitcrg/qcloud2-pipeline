75f9380f7c913530bd2e1f805be8a7c5993c4ae2
Roger Olivella <roger.olivella@crg.eu>
2026-07-01 16:53:48 +0200

    Fix hardcoded http:// prefix on API URLs and simplify Slurm QOS/container config
    - Knime.groovy: stop forcing http:// in front of token/insert-file/insert-data
      URLs, which broke requests when the configured URL already had its own
      scheme.
    - nextflow.config: switch clusterOptions to a fixed --qos=core, reduce
      big_mem to 32G/4h, and bump the qcloud container to 2.2.
    
    Already running fine in production.

--- lib/Knime.groovy
+++ lib/Knime.groovy
@@ -47,10 +47,9 @@
 	 *  Sorting bam files with samtools
  	 */	
 
-
     def public assemble() {
         def string = "knime --launcher.suppressErrors -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -reset -nosave "
-    	string +=  "-workflowFile=${this.wf} "
+        string +=  "-workflowFile=${this.wf} "
     	if (this.mzml) {string +=  "-workflow.variable=input_mzml_file,${this.mzml},String " }
     	if (this.qcml) {string +=  "-workflow.variable=input_qcml_file,${this.qcml},String " }
     	if (this.idxml) {string += "-workflow.variable=input_idxml_file,${this.idxml},String " }
@@ -65,9 +64,9 @@
     	if (this.ijfile)  {string +=  "-workflow.variable=input_json_filename,${this.ijfile},String " }
     	if (this.stype) {string +=  "-workflow.variable=input_sample_type,${this.stype},String " }
     	if (this.labs) {string +=  "-workflow.variable=input_string_labsystem,${this.labs},String " }
-    	if (this.utoken) {string +=  "-workflow.variable=input_url_token,http://${this.utoken},String " }
-    	if (this.uifile) {string +=  "-workflow.variable=input_url_insert_file,http://${this.uifile},String " }
-    	if (this.uidata) {string +=  "-workflow.variable=input_url_insert_data,http://${this.uidata},String " }
+    	if (this.utoken) {string +=  "-workflow.variable=input_url_token,${this.utoken},String " }
+    	if (this.uifile) {string +=  "-workflow.variable=input_url_insert_file,${this.uifile},String " }
+    	if (this.uidata) {string +=  "-workflow.variable=input_url_insert_data,${this.uidata},String " }
 		string += "-workflow.variable=output_json_folder,${this.ofolder},String "
 		string += "-workflow.variable=input_json_folder,${this.ifolder},String "
     	if (this.ofeatxml) {string +=  "-workflow.variable=output_featurexml_file,${this.ofeatxml},String " }
